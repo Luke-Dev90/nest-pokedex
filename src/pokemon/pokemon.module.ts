b1636b3f6c32a41a56a4441dddc3815635b1695c
lucaschalela990 <lukeesf2016@gmail.com>
2022-08-31 00:24:42 -0300

    v1 project pokedex

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,19 @@
+import {MongooseModule} from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      }
+    ])
+  ]
+})
+export class PokemonModule {}
